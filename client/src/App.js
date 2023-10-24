55e794edb1d36a0e6fb880b0066f98fb6069751b
Ankur Kumar Singh <iankurkumarsingh@gmail.com>
2023-10-24 22:53:34 +0530

    aa

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import '../src/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './component/home/Home';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
 import Admin from './component/admin/AdminLogin';
 import AdminDashboard  from './component/admin/AdminDashboard';
 import Category  from './component/category/CategoryAll';
 const App = () => {
   return (    
     <div>
+  <div>
+    <FloatingWhatsApp/>
+  </div>
+  
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}>
